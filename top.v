bb7d64942eeb7eecba336f2490c3c966d6007a63
Cyril-Jean <cyril.s.jean@gmail.com>
2019-07-28 10:07:00 +0100

    Added APB interface.

--- top.v
+++ top.v
@@ -4,47 +4,66 @@
 
 module top
   (
-   // Declare some signals so we can see how I/O works
-   input         clk,
-//   input         fastclk,
-   input         reset_l,
+    // Declare some signals so we can see how I/O works
+    input         clk,
+    input         reset_l,
 
-   output        dcc_out
-   );
+    // APB slave interface
+    input         presetn,
+    input         psel,
+    input         penable,
+    input         pwrite,
+    output        pslverr,
+    output        pready,
+    input  [11:0] paddr,
+    input  [31:0] pwdata,
+    output [31:0] prdata,
 
-   reg [31:0] elapsed_clock;
-   reg        reset_sync;
+    //
+    output        dcc_out
+  );
 
-   always @(posedge clk)
-     begin
-       if (reset_l)
-         reset_sync <= 1'b1;
-       else
-         reset_sync <= 1'b0;
+  reg [31:0] elapsed_clock;
+  reg        reset_sync;
+
+  always @(posedge clk)
+    begin
+      if (reset_l)
+        reset_sync <= 1'b1;
+      else
+        reset_sync <= 1'b0;
      end
 
-   // DCC module
-   dcc dcc (.clk(clk),
-            .reset_n(reset_sync),
-            .track_out(dcc_out));
+  // APB DCC controller
+  apb_dcc_ctrl apb_dcc_ctrl1 (.presetn(reset_l),
+                              .pclk(clk),
+                              .psel(psel),
+                              .penable(penable),
+                              .pwrite(pwrite),
+                              .pslverr(pslverr),
+                              .pready(pready),
+                              .paddr(paddr),
+                              .pwdata(pwdata),
+                              .prdata(prdata),
+                              .dcc_out(dcc_out));
 
-   // Print some stuff as an example
-   initial begin
-      $display("[%0t] Model running...\n", $time);
-   end
+  // Print some stuff as an example
+  initial begin
+    $display("[%0t] Model running...\n", $time);
+  end
 
-   // Lazy test duration control. Run for a specific number of cock cycles
-   always @ (posedge clk)
-     begin
-       if (reset_l == 1'b0)
-         begin
-           elapsed_clock <= 32'b0;
-         end
-       else
-         begin
-           elapsed_clock <= elapsed_clock + 1;
-           if (elapsed_clock[11] == 1'b1) $finish;
-         end
-     end
+  // Lazy test duration control. Run for a specific number of cock cycles
+  always @ (posedge clk)
+    begin
+      if (reset_l == 1'b0)
+        begin
+          elapsed_clock <= 32'b0;
+        end
+      else
+        begin
+          elapsed_clock <= elapsed_clock + 1;
+          if (elapsed_clock[11] == 1'b1) $finish;
+        end
+    end
 
 endmodule
