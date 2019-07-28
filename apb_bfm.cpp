bfd76f8dd4044f5606a05aa2c36269542d27a6d9
Cyril-Jean <cyril.s.jean@gmail.com>
2019-07-28 10:03:25 +0100

    Added basic APB bus bus functional model to drive the system from Verilator test bench.

---
+++ apb_bfm.cpp
@@ -0,0 +1,80 @@
+// MIT license
+// Copyright (c) 2019 Cyril Jean
+// SPDX-License-Identifier: MIT
+
+#include "apb_bfm.h"
+
+ApbBfm::ApbBfm(Vtop * in_top) {
+    top = in_top;
+
+    state = IDLE;
+    top->psel = 0;
+    top->penable = 0;
+    top->pwrite = 0;
+    top->pwdata = 0x00000000;
+};
+
+ApbBfm::~ApbBfm() {
+};
+
+void ApbBfm::write(uint32_t address, uint32_t value) {
+    curr_address = address;
+    curr_value = value;
+    state = WRITE_SETUP;
+};
+
+uint32_t ApbBfm::read(uint32_t address) {
+    curr_address = address;
+    state = READ_SETUP;
+    return 0;
+};
+
+void ApbBfm::drive_bus(int clock) {
+    switch(state) {
+        case IDLE:
+            if(!top->clk) {
+                top->psel = 0;
+                top->penable = 0;
+            };
+            break;
+
+        case WRITE_SETUP:
+            if(!top->clk) {
+                top->paddr = curr_address;
+                top->pwdata = curr_value;
+                top->pwrite = 1;
+                top->psel = 1;
+                state = WRITE_ACCESS;
+            };
+            break;
+
+        case WRITE_ACCESS:
+            if(!top->clk) {
+                top->penable = 1;
+
+                state = IDLE;
+            };
+            break;
+
+        case READ_SETUP:
+            if(!top->clk) {
+                top->pwrite = 0;
+                top->paddr = curr_address;
+                top->psel = 1;
+                state = READ_ACCESS;
+            }
+            break;
+
+        case READ_ACCESS:
+            if(!top->clk) {
+                top->penable = 1;
+                state = IDLE;
+            };
+            break;
+
+        default:
+            state = IDLE;
+            break;
+    };
+};
+
---
+++ apb_bfm.h
@@ -0,0 +1,38 @@
+// MIT license
+// Copyright (c) 2019 Cyril Jean
+// SPDX-License-Identifier: MIT
+
+#ifndef APB_BFM
+#define APB_BFM
+
+#include "Vtop.h"
+
+#include <stdint.h>
+
+class ApbBfm {
+private:
+    enum ApbState {
+        IDLE,
+        WRITE_SETUP,
+        WRITE_ACCESS,
+        READ_SETUP,
+        READ_ACCESS
+    };
+
+    ApbState state;
+
+    uint32_t curr_address;
+    uint32_t curr_value;
+    Vtop * top;
+
+public:
+    ApbBfm(Vtop * top);
+    ~ApbBfm();
+
+    void write(uint32_t address, uint32_t value);
+    uint32_t read(uint32_t address);
+    void drive_bus(int clock_state);
+};
+
+#endif
+
